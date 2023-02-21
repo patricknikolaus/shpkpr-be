86d6eb5d441a1a156c615d1023af80d32b4eae05
patricknikolaus <pdn6991@gmail.com>
2023-02-21 08:53:17 -0500

    init

---
+++ db/migrate/20230218015515_create_wishlists.rb
@@ -0,0 +1,10 @@
+class CreateWishlists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :wishlists do |t|
+      t.integer :game_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
